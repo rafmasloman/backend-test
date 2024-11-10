9d2bd733236e2875d6e6e954f565d8ed5419bc88
rafmasloman <raflymasloman20@gmail.com>
2024-11-10 14:55:08 +0800

    feat : create user service, controller, and routes

---
+++ src/routes/user-routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"backend/api/src/controllers"
+	"backend/api/src/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RouterSetup() *gin.Engine {
+
+	router := gin.Default()
+
+	router.Use(middleware.CorsMiddleware())	
+
+	
+	userRoutes := router.Group("/users") 
+	{
+		userRoutes.GET("/", controllers.GetAllUsers)
+		userRoutes.POST("/", controllers.CreateUser)
+		userRoutes.GET("/:id", controllers.GetDetailUser)
+		userRoutes.PUT("/:id", controllers.UpdateUser)
+		userRoutes.DELETE("/:id", controllers.DeleteUser)
+	}
+
+	return router
+}
